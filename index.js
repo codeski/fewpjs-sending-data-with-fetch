9c1ed6d8fd64886e5b2882fe74b67129c27ddd5c
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-03-25 14:35:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`
+        })
+    })    
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(object) {
+        // console.log(object)
+        // console.log(object.id)
+        const body = document.body
+        const li = document.createElement('li')
+        li.innerText = `${object.id}`
+        body.append(li)
+    })
+    .catch(function(error) {
+        alert("Error!")
+        console.log(error.message)
+    })
+
+}
